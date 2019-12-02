fab62305b91dc5440901bf49a24571a8a24d2d35
eliash2 <55029246+eliash2@users.noreply.github.com>
2019-12-02 15:39:04 -0600

    Back Button works for SettingsActivity

--- app/src/main/java/com/example/hydrate/MapsActivity.java
+++ app/src/main/java/com/example/hydrate/MapsActivity.java
@@ -90,22 +90,14 @@ public class MapsActivity extends FragmentActivity implements GoogleMap.OnMyLoca
                 == PackageManager.PERMISSION_GRANTED) {
             map.setMyLocationEnabled(true);
         } else {
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-                    Manifest.permission.ACCESS_FINE_LOCATION)) {
-                // Show an explanation to the user *asynchronously* -- don't block
-                // this thread waiting for the user's response! After the user
-                // sees the explanation, try again to request the permission.
-            } else {
-                // No explanation needed; request the permission
-                ActivityCompat.requestPermissions(this,
-                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                        MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+            // No explanation needed; request the permission
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
 
-                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
-                // app-defined int constant. The callback method gets the
-                // result of the request.
-            }
-
+            // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+            // app-defined int constant. The callback method gets the
+            // result of the request.
         }
         map.setOnMyLocationButtonClickListener(this);
         map.setOnMyLocationClickListener(this);
