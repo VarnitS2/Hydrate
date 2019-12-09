a5c921bfa7e565f62c824cb4a8273f46c7605bbb
VarnitS2 <varnits02@gmail.com>
2019-12-09 14:37:32 -0600

    Created log in activity; commit before integrating Google Firebase Authentication

--- app/src/main/java/com/example/hydrate/SettingsActivity.java
+++ app/src/main/java/com/example/hydrate/SettingsActivity.java
@@ -1,9 +1,12 @@
 package com.example.hydrate;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 
 public class SettingsActivity extends AppCompatActivity {
@@ -26,6 +29,15 @@ public class SettingsActivity extends AppCompatActivity {
         @Override
         public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey);
+
+            Preference button = findPreference(getString(R.string.log_in));
+            button.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+                @Override
+                public boolean onPreferenceClick(Preference preference) {
+                    startActivity(new Intent(getContext(), LogInActivity.class));
+                    return true;
+                }
+            });
         }
     }
 }
